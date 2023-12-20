b83cf91af3dfbf27c73c4c4c8b746f0ce47a8882
DenisGonchar <denisgonchar123@gmail.com>
2023-12-20 15:58:03 +0300

    Initial commit

---
+++ Source/CameraModes/CameraModesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CameraModesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACameraModesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACameraModesGameMode();
+};
+
+
+
